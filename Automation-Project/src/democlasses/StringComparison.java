e2a85d34abd4f040a33641b4edd582b7c36bf582
brijesh-yadav <brijesh9971@gmail.com>
2016-03-01 20:16:39 +0530

    "Amend String comparison class"

--- Automation-Project/src/democlasses/StringComparison.java
+++ Automation-Project/src/democlasses/StringComparison.java
@@ -7,22 +7,12 @@ public class StringComparison {
 		String s1 ="Hello";
 		String s2 = "Hello";
 		String s3 = new String("Hello");
-		String s4 = new String("Hello");
 		
-		System.out.println(s1+ "");
-		
-		
-		System.out.println(s1+ "");
-		
-		System.out.println(s1+ "");
-		
-		/*
 		if(s1.equals(s2)){
 			System.out.println("match");
 		}else {
 			System.out.println("not match");
 		}
-		*/
 		
 		if(s1.equals(s3)){
 			System.out.println("match");
